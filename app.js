9093b51878283c357cd337ec21693ff46eb11430
naylinhtun1994 <naylin.htun@yathar.com>
2020-10-11 21:11:59 +0630

    Tow way data binding, Computd properties

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 new Vue({
     el: '#vue-app',
     data: {
+        country: '',
+        phone: '',
         name: 'Aung Aung',
         job: 'Web Development',
         firstMethodAge: 20,
@@ -8,6 +10,9 @@ new Vue({
         thirdMethodAge: 20,
         x: 0,
         y: 0,
+        a: 0,
+        b: 0,
+        age: 20,
         website: 'https://www.google.com',
         websiteTag: '<a href="https://www.google.com">Inside bind js for html tag.</a>'
     },
@@ -47,6 +52,36 @@ new Vue({
 
         logAge: function(){
             console.log('You entered your age');
+        },
+
+        logCountry: function(){
+            console.log('You entered your country');
+        },
+
+        logPhone: function(){
+            console.log('You entered your phone number');
+        },
+
+        // addToA: function(){
+        //     console.log('addToA');
+        //     return this.a + this.age;
+        // },
+
+        // addToB: function(){
+        //     console.log('addToB');
+        //     return this.b + this.age;
+        // }
+    },
+
+    computed: {
+        addToA: function(){
+            console.log('addToA');
+            return this.a + this.age;
+        },
+
+        addToB: function(){
+            console.log('addToB');
+            return this.b + this.age;
         }
     }
 });
